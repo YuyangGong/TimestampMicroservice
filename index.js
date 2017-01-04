92d4540ef5fd0fa1d019fbe24dceb9860c5bffd9
yuyanggong <421736079@qq.com>
2017-01-04 23:15:41 +0800

    add routin

--- index.js
+++ index.js
@@ -13,6 +13,23 @@ app.get('/', function(req, res) {
 	});
 });
 
+app.get('/:date', function(req, res) {
+	function dateStringify(dateObj) {
+		var months = [];
+		return `${months[dateObj.getMonth()]} ${dateObj.getDate()}, ${dateObj.getFullYear()}`
+	}
+	var date = req.params.date,
+		time;
+	time = new Date(isNaN(+date) ? date : +date);
+	if(time.toString() === 'Invalid Date') res.json({"unix": null, "natural": null});
+	else res.json({"unix": +time, "natural": dateStringify(time)});
+});
+
+app.get('*', function(req, res) {
+	res.send('404 not found!');
+})
+
 app.listen(port);
+
 
 console.log('run')

acf1550c86d51396e586f3ec2c71d3a34f14fb60
yuyanggong <421736079@qq.com>
2017-01-04 22:51:10 +0800

    change url

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+	fs = require('fs'),
+	app = new express(),
+	port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.writeHeader(200, {'Content-Type': 'text/html'});
+	fs.readFile('./view/pages/index.html', function(err, data) {
+		if(err) throw err;
+		res.end(data);
+	});
+});
+
+app.listen(port);
+
+console.log('run')
